507d4fe4dd4575501f9412ff4a5354244f91da4f
jerenyaks <jeremynm69@gmail.com>
2026-04-11 11:54:50 +0300

    zz.py

---
+++ zz.py
@@ -0,0 +1,9 @@
+def is_even(n):
+    """Return True if n is even, False otherwise."""
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+print(is_even(4))  # Output: True
+print(is_even(7))  # Output: False
+        
