c9970a7054c6a716a34229ad853119c5972cdb38
jerenyaks <jeremynm69@gmail.com>
2026-04-14 21:00:28 +0300

    git pull origin main

--- zz.py
+++ zz.py
@@ -1,3 +1,20 @@
+
+def is_even(n):
+    if n % 2 == 0:
+        return True
+        print("It is even!")
+    else:
+        return False
+        print("It is odd!")
+        #The print statements will never be executed because they come after return statements therefore they are impure
+        #the prints are basically dead code because they will never be executed and they will not display whether the number is even or odd.
+        #if/else statements are not impure but the print statements after the return statements are impure because they will never be executed and they will not display whether the number is even or odd.
+
+
+
+
+
+
 def is_even(n):
     """Return True if n is even, False otherwise."""
     if n % 2 == 0:
